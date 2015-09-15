384db443f3e84df17d844386f516f5578f57f5f2
kmaho <portafortuna.815@gmail.com>
2015-09-15 05:27:09 +0000

    fin

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,4 +4,6 @@ class Message < ActiveRecord::Base
     validates :name , length: {  maximum: 20 } , presence: true
     # 内容は必須入力かつ2文字以上30文字以下
     validates :body , length: {minimum: 2 , maximum: 30 } , presence: true
+    #年齢は必須入力かつ0-200の数字のみ
+    validates :age , inclusion: { in: 0..200 } , numericality: true
 end
